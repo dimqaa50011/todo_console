b041d8bd41c3c63fc36f8267a875504c5984ffb2
Dmitry <dz5011@yandex.ru>
2023-02-28 10:25:53 +0300

    create halpers function

---
+++ app/halpers.py
@@ -0,0 +1,43 @@
+import re
+from typing import List
+from datetime import datetime
+
+from .db_api import OutTaskSchema
+
+DATE_FORMAT_PATTERN = re.compile(r'(\d{4}\-\d{2}\-\d{2})')
+
+
+def get_card(task: OutTaskSchema) -> str:
+    if isinstance(task, OutTaskSchema):
+        return '\n'.join(
+                    (
+                        f'ID: {task.id}',
+                        f'ЗАголовок: {task.title}',
+                        'Тело заметки отсутсвует' if task.body is None else f'Зaметка: {task.body}',
+                        f'Время создания: {datetime.strftime(task.created_at, "%d %b %Y %H:%M")}',
+                        f'Время последнего обновления: {datetime.strftime(task.updated_at, "%d %b %Y %H:%M")}',
+                    )
+                )
+
+
+def get_all_cards(data: List[OutTaskSchema]) -> List:
+    if isinstance(data, list) and isinstance(data[0], OutTaskSchema):
+        return [get_card(task) for task in data]
+
+
+def get_pk():
+    pk = input('Введите ID задачи: ').strip()
+    return int(pk) if pk.isdigit() else None
+
+
+def _clean_date(date: str) -> str:
+    return re.sub(r'[^\-&\d]+', '-', date)
+
+
+def get_search_date(msg: str):
+    search_date = input(msg).strip()
+    search_date = _clean_date(search_date)
+    print()
+    if re.match(DATE_FORMAT_PATTERN, search_date):
+        return datetime.strptime(search_date, '%Y-%m-%d')
+    return None
